f86613669d857a14157a4938d6f8aa7e03297f3e
leoespeu <btssioleo.espeu@gmail.com>
2018-04-26 19:25:53 +0200

    -Création des composants du corps du service . -Ajout d'un timer pour la fréquence d'éxécution du service . -Création d'un journal de log

--- TestWindowService/Service1.cs
+++ TestWindowService/Service1.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace TestWindowService
 {
     public partial class Service1 : ServiceBase
     {
+        private Timer timer1 = null;
+
         public Service1()
         {
             InitializeComponent();
@@ -19,10 +22,22 @@ namespace TestWindowService
 
         protected override void OnStart(string[] args)
         {
+            timer1 = new Timer();
+            this.timer1.Interval = 30000;
+            this.timer1.Elapsed += new System.Timers.ElapsedEventHandler(this.timer1_Tick);
+            timer1.Enabled = true;
+            Library.WriteErrorLog("Test Window service started");
+        }
+
+        private void timer1_Tick(object sender,ElapsedEventArgs e)
+        {
+            Library.WriteErrorLog("Timer ticked and some job has been done successfuly");
         }
 
         protected override void OnStop()
         {
+            this.timer1.Enabled = false;
+            Library.WriteErrorLog("Test window service stopped");
         }
     }
 }
